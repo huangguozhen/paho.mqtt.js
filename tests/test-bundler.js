c096a8d95382b8c03eac09d6a07d15a9d19bc3c8
huangguozhen <huangguozhen@outlook.com>
2016-05-12 10:05:18 +0800

    feat(test): add karma

---
+++ tests/test-bundler.js
@@ -0,0 +1,28 @@
+// ---------------------------------------
+// Test Environment Setup
+// ---------------------------------------
+import chai from 'chai'
+import chaiAsPromised from 'chai-as-promised'
+
+chai.use(chaiAsPromised)
+
+global.chai = chai
+global.expect = chai.expect
+global.should = chai.should()
+
+// ---------------------------------------
+// Require Tests
+// ---------------------------------------
+// for use with karma-webpack-with-fast-source-maps
+// NOTE: `new Array()` is used rather than an array literal since
+// for some reason an array literal without a trailing `;` causes
+// some build environments to fail.
+const __karmaWebpackManifest__ = new Array() // eslint-disable-line
+const inManifest = (path) => ~__karmaWebpackManifest__.indexOf(path)
+
+// require all `tests/**/*.spec.js`
+const testsContext = require.context('./', true, /\.spec\.js$/)
+
+// only run tests that have changed after the first pass.
+const testsToRun = testsContext.keys().filter(inManifest)
+;(testsToRun.length ? testsToRun : testsContext.keys()).forEach(testsContext)
